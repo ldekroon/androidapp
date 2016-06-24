25d66773e97e6b033f5a36f14966f3a1a54753b8
Craz1k0ek <bram_kolkman@hotmail.com>
2016-06-24 17:25:07 +0200

    Init
    init

---
+++ TestApp/app/src/main/java/com/wordpress/craz1k0ek/testapp/ActivationActivity.java
@@ -0,0 +1,49 @@
+package com.wordpress.craz1k0ek.testapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
+public class ActivationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activation);
+
+        //Prevents autofocus on startup
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+    /*
+    24/6/2016 - Bram Kolkman
+    Action performed when txtActHelp is clicked. Opens the link issued in the Uri.parse.
+    */
+    public void open_help(View view) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/"));
+        startActivity(browserIntent);
+    }
+
+    public void small_Logo(View view) {
+        ImageView v = (ImageView) findViewById(R.id.imgLog);
+        v.getLayoutParams().height = 0;
+        v.getLayoutParams().width = 0;
+        Log.d("INFO", "Smaller Logo");
+    }
+
+    public void big_Logo(View view) {
+        ImageView v = (ImageView) findViewById(R.id.imgLog);
+        v.getLayoutParams().height = -2;
+        v.getLayoutParams().width = -2;
+        Log.d("INFO", "Biggen Logo");
+    }
+
+    public boolean onKeyDown(int keyCode, keyEvent event) {
+
+    }
+}
